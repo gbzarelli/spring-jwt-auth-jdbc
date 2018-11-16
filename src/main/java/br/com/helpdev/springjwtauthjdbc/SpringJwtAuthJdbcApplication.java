d92c45fa438d02ff8f3dcfd59918e0430962a453
Guilherme Biff Zarelli <guilhermeb@grupocriar>
2018-11-16 15:23:51 -0200

    init

---
+++ src/main/java/br/com/helpdev/springjwtauthjdbc/SpringJwtAuthJdbcApplication.java
@@ -0,0 +1,12 @@
+package br.com.helpdev.springjwtauthjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtAuthJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJwtAuthJdbcApplication.class, args);
+    }
+}
